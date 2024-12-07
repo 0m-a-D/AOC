ed0a65bb13f71d4307055255a9c1ccbc5c0ea5ef
Om Anand <omishere21@gmail.com>
2024-12-07 22:53:10 +0530

    call part1_sol and part2_sol from lib crate

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,4 +1,5 @@
 #![allow(unused)]
+use day1::{part1, part2};
 
 use std::{fs, path::Path};
 fn main() {
@@ -9,7 +10,6 @@ fn main() {
     let mut vec1 = Vec::with_capacity(1000);
     let mut vec2 = Vec::with_capacity(1000);
 
-    // part 1 solution
     for line in contents {
         let num1: i32 = line[..5].parse().expect("failed to parse to i32");
         let num2: i32 = line[8..].parse().expect("failed to parse to i32");
@@ -18,27 +18,12 @@ fn main() {
     }
     vec1.sort();
     vec2.sort();
-    let mut total = 0;
-    for i in 0..1000 {
-        let diff1 = vec1[i];
-        let diff2 = vec2[i];
-        let diff = (diff1 - diff2).abs();
-        total += diff;
-    }
-    println!("{total}");
+
+    // part 1 solution
+    let part1_sol = part1(&vec1, &vec2);
+    println!("{part1_sol}");
 
     // part 2 solution
-    let mut another_total = 0;
-    for i in 0..1000 {
-        let mut counter = 0;
-        if vec2.contains(&vec1[i]) {
-            for j in 0..(vec2.len()) {
-                if vec1[i] == vec2[j] {
-                    counter += 1;
-                }
-            }
-        }
-        another_total += vec1[i] * counter;
-    }
-    println!("{another_total}");
+    let part2_sol = part2(vec1, vec2);
+    println!("{part2_sol}");
 }
